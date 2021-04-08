c9bdd496103615df87aa5cbe6411c7438c691a28
Miro Dojkic <mdojkic@gmail.com>
2021-04-08 18:42:04 +0200

    Enable debug config option 🔧

--- src/database/config.ts
+++ src/database/config.ts
@@ -1,8 +1,11 @@
+import yn from 'yn';
+import Organisation from '../organisation/model';
 import User from '../user/model';
 import { PostgreSqlDriver } from '@mikro-orm/postgresql';
 
 export default {
   driver: PostgreSqlDriver,
   clientUrl: process.env.DATABASE_URL,
-  entities: [User]
+  debug: yn(process.env.DEBUG),
+  entities: [Organisation, User]
 };
